1798d8fe8fd3faeeba0c21be0d28c67ad805e7ab
Rayhan Yovi <yovihan@gmail.com>
2023-09-09 23:41:52 +0700

    IT'S FINALLY WORKING!

--- src/Dijkstra.js
+++ src/Dijkstra.js
@@ -1,8 +1,9 @@
-// Given that you only need unweighted paths, a BFS approach is sufficient
-function bfs(start, targets) {
+// Dijkstra.js
+
+// Existing BFS function with small modifications to return path between two points.
+function bfs(start, end) {
   const visited = new Set();
   const queue = [{ ...start, path: [start] }];
-  const allPaths = [];
 
   while (queue.length > 0) {
     const currentNode = queue.shift();
@@ -12,41 +13,70 @@ function bfs(start, targets) {
 
     visited.add(`${x},${y}`);
 
-    // Check if we're on a target node
-    const targetNode = targets.find(
-      (target) => target.x === x && target.y === y
-    );
-    if (targetNode) {
-      allPaths.push(path.concat(targetNode));
-      targets = targets.filter((target) => target.x !== x || target.y !== y);
-      if (targets.length === 0) break; // All targets visited
-    }
+    if (x === end.x && y === end.y) return path; // End the search if we found the target
 
-    // Push neighbors to queue
     const neighbors = [
-      { x: x + 1, y }, // Right
-      { x: x - 1, y }, // Left
-      { x, y: y + 1 }, // Down
-      { x, y: y - 1 }, // Up
-      { x: x + 1, y: y + 1 }, // Bottom right diagonal
-      { x: x - 1, y: y - 1 }, // Top left diagonal
-      { x: x + 1, y: y - 1 }, // Top right diagonal
-      { x: x - 1, y: y + 1 }, // Bottom left diagonal
+      { x: x + 1, y },
+      { x: x - 1, y },
+      { x, y: y + 1 },
+      { x, y: y - 1 },
+      { x: x + 1, y: y + 1 },
+      { x: x - 1, y: y - 1 },
+      { x: x + 1, y: y - 1 },
+      { x: x - 1, y: y + 1 },
     ].filter(
       (neigh) => neigh.x >= 0 && neigh.x < 50 && neigh.y >= 0 && neigh.y < 50
     );
+
     for (const neighbor of neighbors) {
       if (!visited.has(`${neighbor.x},${neighbor.y}`)) {
         queue.push({ ...neighbor, path: path.concat(neighbor) });
       }
     }
   }
+  return []; // If no path found
+}
 
-  // Merge the paths (for now, just join them, there are more efficient ways to do this)
-  const fullPath = allPaths.reduce((acc, currPath) => acc.concat(currPath), []);
-  return fullPath;
+// Utility function to compute permutations of an array
+function permute(array) {
+  if (array.length === 0) return [[]];
+  const firstElement = array[0];
+  const remainingElements = array.slice(1);
+  const permsWithoutFirst = permute(remainingElements);
+  return permsWithoutFirst.reduce((acc, perm) => {
+    for (let i = 0; i <= perm.length; i++) {
+      const permWithFirst = [
+        ...perm.slice(0, i),
+        firstElement,
+        ...perm.slice(i),
+      ];
+      acc.push(permWithFirst);
+    }
+    return acc;
+  }, []);
 }
 
 export function dijkstra(start, targets) {
-  return bfs(start, targets);
+  const allOrders = permute(targets);
+  let shortestPath = [];
+  let shortestLength = Infinity;
+
+  for (let order of allOrders) {
+    let pathLength = 0;
+    let currentPath = [];
+    let current = start;
+
+    for (let target of order) {
+      const segment = bfs(current, target);
+      pathLength += segment.length;
+      currentPath = currentPath.concat(segment);
+      current = target;
+    }
+
+    if (pathLength < shortestLength) {
+      shortestLength = pathLength;
+      shortestPath = currentPath;
+    }
+  }
+  return shortestPath;
 }
